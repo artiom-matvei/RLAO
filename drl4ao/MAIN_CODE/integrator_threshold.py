ac0a156e81590d1d661feed635bf13cca9799854
Parker Levesque <parker.levesque@gmail.com>
2024-09-12 16:23:29 -0400

    Update to thesholding plots

--- drl4ao/MAIN_CODE/integrator_threshold.py
+++ drl4ao/MAIN_CODE/integrator_threshold.py
@@ -8,17 +8,17 @@ import os,sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
-# from parser_Configurations import Config, ConfigAction
-# from OOPAOEnv.OOPAOEnvRazor import OOPAO
-from PO4AO.util_simple import read_yaml_file #TorchWrapper, 
+from ML_stuff.dataset_tools import read_yaml_file #TorchWrapper, 
 from Plots.plots import save_plots
 # import matplotlib.pyplot as plt
 # import argparse
 import time
 import numpy as np
+import torch
 from PO4AO.mbrl_funcsRAZOR import get_env
 from types import SimpleNamespace
 import matplotlib.pyplot as plt
+from cycler import cycler
 
 # SimpleNamespace takes a dict and allows the use of
 # keys as attributes. ex: args['r0'] -> args.r0
@@ -225,12 +225,17 @@ for mag in mags:
 # plt.imshow(after - before)
 # plt.show()
 # %%
-data = torch.load('/home/parker09/projects/def-lplevass/parker09/drl4papyrus/logs/many_threshold/integrator/20240907-172744_gains_magnitude_4.5_gain_0.2/sr2plot.pt')
+path_to_logs = '/Users/parkerlevesque/School/Research/AO/RLAO/logs'
+
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 9))
+
+data = torch.load(path_to_logs + '/many_threshold/integrator/20240907-172744_gains_magnitude_4.5_gain_0.2/sr2plot.pt')
 
 arr = np.array(data).reshape(6,10)
 srs = np.mean(arr, axis=1)
 
-wfs_raw = np.load('/home/parker09/projects/def-lplevass/parker09/drl4papyrus/logs/many_threshold/integrator/20240907-172744_gains_magnitude_4.5_gain_0.2/raw_wfs.npy')
+wfs_raw = np.load(path_to_logs + '/many_threshold/integrator/20240907-172744_gains_magnitude_4.5_gain_0.2/raw_wfs.npy')
 
 
 wfs_max = np.max(wfs_raw)
@@ -239,7 +244,7 @@ ot = -wfs_min/wfs_max
 
 threshs = np.linspace(0.01, ot, 6)
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 9))  # 2 rows, 1 column
+  # 2 rows, 1 column
 
 # First plot (top)
 ax1.plot(threshs*wfs_max, np.mean(arr, axis=1))
@@ -267,5 +272,74 @@ ax3.set_title('Zoomed Out Version')
 plt.tight_layout()
 
 # Display the plot
+plt.show()
+# %%
+##### MAKING THE BIG POT #####
+
+# Custom color scheme inspired by 'inferno'
+custom_colors = ['#440154', '#482878', '#F0701D', '#FF4500', '#FDE724']
+
+# Set the color cycle globally for all plots
+
+plt.style.use('seaborn-v0_8')
+plt.rc('axes', prop_cycle=cycler('color', custom_colors))
+
+exp = '/Users/parkerlevesque/School/Research/AO/RLAO/logs/many_threshold_plus/many_threshold_plus/integrator'
+
+x = []
+
+dirs = os.listdir(exp)
+
+idx = np.argsort([float(x[-12:-9]) for x in dirs])
+
+fig, ax = plt.subplots(3, len(dirs), figsize=(4*len(dirs), 9))
+for j, i in enumerate(idx):
+    data = torch.load(exp+'/'+dirs[i]+'/sr2plot.pt')
+
+    arr = np.array(data).reshape(8,10)
+    srs = np.mean(arr, axis=1)
+    wfs_raw = np.load(exp+'/'+dirs[i]+'/raw_wfs.npy')
+    magnitude = float(dirs[i][-12:-9])
+
+
+    wfs_max = np.max(wfs_raw)
+    wfs_min = np.min(wfs_raw)
+    ot = -wfs_min/wfs_max
+
+    threshs = np.linspace(0.01, ot, 6)
+
+    step_size = threshs[1] - threshs[0]
+
+    last_value = threshs[-1]
+
+    thresh_mesh = np.append(threshs, [last_value + step_size, last_value + 2 * step_size])
+
+      # 2 rows, 1 column
+
+    # First plot (top)
+    ax[0,j].plot(thresh_mesh*wfs_max, np.mean(arr, axis=1), c=custom_colors[1])
+    ax[0,j].set_title(f'Mean Strehl -- Mag {magnitude}')
+
+    # Second plot (middle)
+    ax[1,j].hist(wfs_raw.flatten(), bins=np.arange(wfs_min, wfs_max), color=custom_colors[1])
+    for i in thresh_mesh:
+        ax[1,j].axvline(i*wfs_max, c=custom_colors[3])
+    ax[1,j].set_yscale('log')
+    ax[1,j].set_title('Threshold location')
+
+    ax[0,j].set_xlim(thresh_mesh[0]*wfs_max - 1, thresh_mesh[-1]*wfs_max + 1)
+    ax[1,j].set_xlim(thresh_mesh[0]*wfs_max - 1, thresh_mesh[-1]*wfs_max + 1)
+
+    # Third Plot (bottom)
+    ax[2,j].hist(wfs_raw.flatten(), bins=np.arange(wfs_min, wfs_max + 2), color=custom_colors[1])
+    for i in thresh_mesh:
+        ax[2,j].axvline(i*wfs_max, c=custom_colors[3])
+    ax[2,j].set_yscale('log')
+    ax[2,j].set_title('Zoomed Out Version')
+
+
+plt.ylabel('Strehl Ratio')
+# plt.legend()
+# plt.savefig('../../logs/'+args.savedir+'/integrator/result.png')
 plt.show()
 # %%
