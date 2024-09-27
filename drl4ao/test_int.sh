1da0bd919b71fe8d0c554b45c75e55ea5b834091
Parker Levesque <parker09@narval3.narval.calcul.quebec>
2024-09-27 12:05:16 -0400

    Testing new script for network

--- drl4ao/test_int.sh
+++ drl4ao/test_int.sh
@@ -3,13 +3,13 @@
 #SBATCH --cpus-per-task=2 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=40G                        # memory per node
-#SBATCH --time=00-05:59         # time (DD-HH:MM)
+#SBATCH --time=00-00:59         # time (DD-HH:MM)
 #SBATCH --account=def-lplevass
-#SBATCH --job-name=multiple_gains
+#SBATCH --job-name=network+integrator
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=parker.levesque@gmail.com
 
 cd MAIN_CODE
 
 source $HOME/projects/def-lplevass/parker09/drl4ao_env/bin/activate
-python $HOME/projects/def-lplevass/parker09/drl4papyrus/drl4ao/MAIN_CODE/integrator_oopao_razor.py
+python $HOME/projects/def-lplevass/parker09/drl4papyrus/drl4ao/MAIN_CODE/integrator_network.py
