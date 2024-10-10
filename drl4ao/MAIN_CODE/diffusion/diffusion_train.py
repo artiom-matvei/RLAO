05dc475acc391468962954aa69ae4a3f276fd5e3
Parker Levesque <parker09@narval3.narval.calcul.quebec>
2024-10-09 22:16:45 -0400

    Ready for training

--- drl4ao/MAIN_CODE/diffusion/diffusion_train.py
+++ drl4ao/MAIN_CODE/diffusion/diffusion_train.py
@@ -1,15 +1,37 @@
+
+#%%
 import torch
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 from score_models import ScoreModel, NCSNpp
 
 from data_loading.dataset_tools import DiffusionDataset
 
-HO_file_path = 'datasets/wfs_HO_diff.npy'
-LO_file_path = 'datasets/wfs_LO_diff.npy'
+HO_file_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/diffusion/datasets/wfs_HO_diff.npy'
+LO_file_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/diffusion/datasets/wfs_LO_diff.npy'
 wfs_shape = (48, 48)
 
 dataset = DiffusionDataset(HO_file_path, LO_file_path, wfs_shape, size=400000, use_mmap=True)
 
 net = NCSNpp(channels=4, nf=64, ch_mult=(2, 2, 2), conditions=("input_tensor",), condition_channels=(4,))
 sbm = ScoreModel(net, sigma_min=1e-3, sigma_max=1100)
+
+# hi, lo = dataset.__getitem__(19204)
+
+# frame = torch.zeros((24*4, 24*2))
+# for i in range(4):
+#     frame[24*i: 24*i + 24, :] = torch.cat([hi[i],lo[i]], dim=1)
+
+
+# plt.imshow(np.log10(frame + 1))
+# plt.title('Raw ATM     50 Modes')
+# plt.axis('off')
+# plt.show()
+
+# %%
+checkpoint_dir = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/diffusion/datasets/checkpoints'
+
+sbm.fit(dataset, learning_rate=1e-4, epochs=100000, batch_size=16,\
+        checkpoints=10, checkpoints_directory=checkpoint_dir,\
+        models_to_keep=2)
