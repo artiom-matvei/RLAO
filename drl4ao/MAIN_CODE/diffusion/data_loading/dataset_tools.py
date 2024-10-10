bba6e9449229e6345cb6fc3438bfb6def2955518
Parker Levesque <parker09@narval1.narval.calcul.quebec>
2024-10-09 22:44:20 -0400

    Added device to dataset

--- drl4ao/MAIN_CODE/diffusion/data_loading/dataset_tools.py
+++ drl4ao/MAIN_CODE/diffusion/data_loading/dataset_tools.py
@@ -1,15 +1,16 @@
 import torch
 import numpy as np
 import os
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 
 class DiffusionDataset(Dataset):
-    def __init__(self, HO_file_path, LO_file_path, wfs_shape, size=400000, use_mmap=True):
+    def __init__(self, HO_file_path, LO_file_path, wfs_shape, device, size=400000, use_mmap=True):
         self.HO_file_path = HO_file_path
         self.LO_file_path = LO_file_path
         self.use_mmap = use_mmap
         self.wfs_shape = wfs_shape
         self.size = size
+        self.device = device
 
         if use_mmap:
             self.HO_data = np.memmap(self.HO_file_path, dtype='float32', mode='r', shape=(self.size, *self.wfs_shape))
@@ -32,4 +33,4 @@ class DiffusionDataset(Dataset):
         reshaped_HO = HO_image.view(2, 24, 2, 24).permute(0, 2, 1, 3).contiguous().view(4, 24, 24)
         reshaped_LO = LO_image.view(2, 24, 2, 24).permute(0, 2, 1, 3).contiguous().view(4, 24, 24)
 
-        return reshaped_HO, reshaped_LO
+        return reshaped_HO.to(self.device), reshaped_LO.to(self.device)
--- drl4ao/MAIN_CODE/diffusion/diffusion_train.py
+++ drl4ao/MAIN_CODE/diffusion/diffusion_train.py
@@ -8,11 +8,13 @@ from score_models import ScoreModel, NCSNpp
 
 from data_loading.dataset_tools import DiffusionDataset
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 HO_file_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/diffusion/datasets/wfs_HO_diff.npy'
 LO_file_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/diffusion/datasets/wfs_LO_diff.npy'
 wfs_shape = (48, 48)
 
-dataset = DiffusionDataset(HO_file_path, LO_file_path, wfs_shape, size=400000, use_mmap=True)
+dataset = DiffusionDataset(HO_file_path, LO_file_path, wfs_shape, device, size=400000, use_mmap=True)
 
 net = NCSNpp(channels=4, nf=64, ch_mult=(2, 2, 2), conditions=("input_tensor",), condition_channels=(4,))
 sbm = ScoreModel(net, sigma_min=1e-3, sigma_max=1100)
