c4e03a2c8a8cfc0f53f97fe1d9f3e91baab6d916
Parker Levesque <parker.levesque@gmail.com>
2024-11-06 16:29:45 -0500

    Change minus to make likelihood score attractive

--- drl4ao/MAIN_CODE/diffusion/post_sampling.py
+++ drl4ao/MAIN_CODE/diffusion/post_sampling.py
@@ -15,27 +15,27 @@ hr = np.load(f'{script_dir}/images/hr.npy')
 
 model = ScoreModel(checkpoints_directory=f'{script_dir}/cp_unconditional/')
 
-B = 100
+B = 1
 channels = 4
 
 start_from_y = False
 # %%
-num_steps = 1000
+num_steps = 500
 dt = -1/num_steps #reverse time
 
 s_min = model.sde.sigma_min
 s_max = model.sde.sigma_max
 
-eta = 0.1
+eta = 0.
 
-y = torch.from_numpy(lr).to(device)
+y = torch.from_numpy(lr)[:B].to(device)
 
 x_t = torch.normal(0, s_max, (B, channels, 24, 24)).to(device)
 
 t_start = 1 # if not start_from_y else np.log(np.std(lr[0][0] - hr[0][0])) / np.log(s_max / s_min)
 
-for t in np.linspace(t_start, 0, num_steps):
-    print(t)
+for i, t in enumerate(np.linspace(t_start, 0, num_steps)):
+    print(f'Step {i}/{num_steps}')
     t = torch.tensor(t).to(device) * torch.ones(B).to(device)
     z = torch.randn_like(x_t).to(device)
     dw = abs(dt)**(1/2) * z
@@ -43,7 +43,7 @@ for t in np.linspace(t_start, 0, num_steps):
 
     sig_t = model.sde.sigma(t).to(device).unsqueeze(1).unsqueeze(2).unsqueeze(3)
 
-    score_likelihood = - (y - x_t) / (sig_t ** 2 + eta**2)
+    score_likelihood = (y - x_t) / (sig_t ** 2 + eta**2)
     score_prior = model.score(t, x_t).to(device)
 
     dx = - g**2 * ( score_likelihood + score_prior) * dt + g * dw
