76d6dcee4e4b915114a8442365806cb43d69e0c4
Parker Levesque <parker.levesque@gmail.com>
2024-11-06 15:43:44 -0500

    shape error and load/saving directory automated

--- drl4ao/MAIN_CODE/diffusion/post_sampling.py
+++ drl4ao/MAIN_CODE/diffusion/post_sampling.py
@@ -7,13 +7,15 @@ from score_models import ScoreModel, NCSNpp
 
 # %%
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
 # Print the size in mb of the dataset file
-lr = np.load('/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/images/lr.npy')
-hr = np.load('/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/images/hr.npy')
+lr = np.load(f'{script_dir}/images/lr.npy')
+hr = np.load(f'{script_dir}/images/hr.npy')
 
-model = ScoreModel(checkpoints_directory='/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/cp_unconditional/')
+model = ScoreModel(checkpoints_directory=f'{script_dir}/cp_unconditional/')
 
-B = 1
+B = 100
 channels = 4
 
 start_from_y = False
@@ -33,17 +35,21 @@ x_t = torch.normal(0, s_max, (B, channels, 24, 24)).to(device)
 t_start = 1 # if not start_from_y else np.log(np.std(lr[0][0] - hr[0][0])) / np.log(s_max / s_min)
 
 for t in np.linspace(t_start, 0, num_steps):
+    print(t)
     t = torch.tensor(t).to(device) * torch.ones(B).to(device)
     z = torch.randn_like(x_t).to(device)
     dw = abs(dt)**(1/2) * z
     g = model.sde.diffusion(t, x_t).to(device)
 
-    score_likelihood = - (y - x_t) / (model.sde.sigma(t).to(device) ** 2 + eta**2)
+    sig_t = model.sde.sigma(t).to(device).unsqueeze(1).unsqueeze(2).unsqueeze(3)
+
+    score_likelihood = - (y - x_t) / (sig_t ** 2 + eta**2)
     score_prior = model.score(t, x_t).to(device)
 
     dx = - g**2 * ( score_likelihood + score_prior) * dt + g * dw
 
     x_t += dx
+    
 
-
+torch.save(x_t, f'{script_dir}/images/samples_unc.pt')
 # %%
