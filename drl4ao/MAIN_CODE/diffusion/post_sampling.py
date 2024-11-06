82e8696093d29050edfa1aa783029255f126cb64
Parker Levesque <parker.levesque@gmail.com>
2024-11-06 15:34:29 -0500

    Prep for sampling on cluster

--- drl4ao/MAIN_CODE/diffusion/post_sampling.py
+++ drl4ao/MAIN_CODE/diffusion/post_sampling.py
@@ -5,31 +5,11 @@ import os, sys
 import matplotlib.pyplot as plt
 from score_models import ScoreModel, NCSNpp
 
-from data_loading.dataset_tools import uncondDataset
-
-
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
-from PO4AO.mbrl import get_env
-from ML_stuff.dataset_tools import read_yaml_file
-from types import SimpleNamespace
-try:
-    args = SimpleNamespace(**read_yaml_file('./Conf/papyrus_config.yaml'))
-except:
-    args = SimpleNamespace(**read_yaml_file('../Conf/papyrus_config.yaml'))
-
-
-env = get_env(args)
 # %%
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Print the size in mb of the dataset file
 lr = np.load('/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/images/lr.npy')
 hr = np.load('/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/images/hr.npy')
-
-data = uncondDataset('/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/images/lr.npy', (48,48), device='cpu', size=100)
 
 model = ScoreModel(checkpoints_directory='/Users/parkerlevesque/School/Research/AO/RLAO/drl4ao/MAIN_CODE/diffusion/cp_unconditional/')
 
@@ -46,20 +26,20 @@ s_max = model.sde.sigma_max
 
 eta = 0.1
 
-y = data.__getitem__(0).unsqueeze(0)
+y = torch.from_numpy(lr).to(device)
 
-x_t = torch.normal(0, s_max, (B, channels, 24, 24))
+x_t = torch.normal(0, s_max, (B, channels, 24, 24)).to(device)
 
-t_start = 1 if not start_from_y else np.log(np.std(lr[0][0] - hr[0][0])) / np.log(s_max / s_min)
+t_start = 1 # if not start_from_y else np.log(np.std(lr[0][0] - hr[0][0])) / np.log(s_max / s_min)
 
 for t in np.linspace(t_start, 0, num_steps):
-    t = torch.tensor(t) * torch.ones(B)
-    z = torch.randn_like(x_t)
+    t = torch.tensor(t).to(device) * torch.ones(B).to(device)
+    z = torch.randn_like(x_t).to(device)
     dw = abs(dt)**(1/2) * z
-    g = model.sde.diffusion(t, x_t)
+    g = model.sde.diffusion(t, x_t).to(device)
 
-    score_likelihood = - (y - x_t) / (model.sde.sigma(t) ** 2 + eta**2)
-    score_prior = model.score(t, x_t)
+    score_likelihood = - (y - x_t) / (model.sde.sigma(t).to(device) ** 2 + eta**2)
+    score_prior = model.score(t, x_t).to(device)
 
     dx = - g**2 * ( score_likelihood + score_prior) * dt + g * dw
 
