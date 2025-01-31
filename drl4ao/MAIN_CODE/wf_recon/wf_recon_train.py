f3837fa6c78c1e3d2ee2f954b8de76b516928704
Parker Levesque <parker09@narval1.narval.calcul.quebec>
2025-01-31 11:09:32 -0500

    Wavefront reconstruction training for thesis results

--- drl4ao/MAIN_CODE/wf_recon/wf_recon_train.py
+++ drl4ao/MAIN_CODE/wf_recon/wf_recon_train.py
@@ -42,7 +42,7 @@ savedir = os.path.dirname(__file__)
 env = get_env(args)
 
 
-with open("quick_test.txt", "a") as f:
+with open("thesis_train_filtered.txt", "a") as f:
     f.write(f"Done making env \n")
 
 
@@ -76,17 +76,17 @@ with open("quick_test.txt", "a") as f:
 # ds_size = len(X)
 #------------- Uncomment to load a dataset from individual files -------------#
 
-data_dir_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/wf_recon/datasets/atm_stats'
+data_dir_path = '/home/parker09/projects/def-lplevass/parker09/RLAO/drl4ao/MAIN_CODE/wf_recon/datsets/thesis_data'
 
 # X = os.listdir(data_dir_path + '/inputs')
 # y = os.listdir(data_dir_path + '/targets')
 
-ds_size = 300000
+ds_size = 400000
 
 
 # %%
 # Set the random seed for reproducibility
-np.random.seed(432574358)
+np.random.seed(43257458)
 
 # Shuffle the data indices
 indices = np.arange(ds_size)
@@ -123,22 +123,22 @@ test_indices = indices[train_size + val_size:]
 
 
 #------------- Uncomment for datasets from file names -------------#
-input_file_path = data_dir_path+'/wfs_frames_unmodulated_300.npy'
-target_file_path = data_dir_path+'/dm_cmds_unmodulated_300.npy'
+input_file_path = data_dir_path+'/wfs_frames_filtered.npy'
+target_file_path = data_dir_path+'/dm_cmds.npy'
 
 dm_shape = env.dm.coefs.shape
 wfs_shape=env.wfs.cam.frame.shape
 
-D_train = FileDataset(input_file_path, target_file_path, train_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=300000)
-D_test = FileDataset(input_file_path, target_file_path, test_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=300000)
-D_val = FileDataset(input_file_path, target_file_path, val_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=300000)
+D_train = FileDataset(input_file_path, target_file_path, train_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=ds_size)
+D_test = FileDataset(input_file_path, target_file_path, test_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=ds_size)
+D_val = FileDataset(input_file_path, target_file_path, val_indices, dm_shape=dm_shape, wfs_shape=wfs_shape, size=ds_size)
 
-with open("quick_test.txt", "a") as f:  # 'a' mode appends to the file
+with open("thesis_train_filtered.txt", "a") as f:  # 'a' mode appends to the file
     f.write(f"Done making train, test, val datasets \n")
 
 # %%
 
-checkpoint = torch.load(savedir+'/models/useable/unmod.pt',map_location=device)
+# checkpoint = torch.load(savedir+'/models/useable/unmod.pt',map_location=device)
 
 
 # Make sure to use the correct network before loading the state dict
@@ -147,7 +147,7 @@ reconstructor = Unet_big(env.xvalid,env.yvalid)
 # reconstructor.load_state_dict(checkpoint['model_state_dict'])
 
 # reconstructor = Unet_big(env.xvalid, env.yvalid)
-# reconstructor = reconstructor.to(device)
+reconstructor = reconstructor.to(device)
 
 # EMA of model parameters
 # ema_reconstructor = torch.optim.swa_utils.AveragedModel(reconstructor, \
@@ -171,7 +171,7 @@ val_losses = []
 ema_val_losses = []
 # Variable to store the best validation loss and path to save the model
 best_val_loss = float('inf')  # Initialize to infinity
-save_path = savedir+'/models/tmp/unmod_ft.pt'  # Path to save the best model
+save_path = savedir+'/models/thesis_models/atmos.pt'  # Path to save the best model
 
 
 # Parameters
@@ -188,7 +188,7 @@ weight_matrix = weight_matrix.unsqueeze(0).unsqueeze(0)  # Shape: [1, 1, height,
 # weight_matrix = weight_matrix.expand(batch_size, channels, height, width)
 
 
-with open("quick_test.txt", "a") as f:  # 'a' mode appends to the file
+with open("thesis_train_filtered.txt", "a") as f:  # 'a' mode appends to the file
     f.write(f"Starting Training \n")
 
 
@@ -235,7 +235,7 @@ for epoch in range(n_epochs):
 
     end_tr = time.time()
 
-    with open("quick_test.txt", "a") as f:  # 'a' mode appends to the file
+    with open("thesis_train_filtered.txt", "a") as f:  # 'a' mode appends to the file
         f.write(f"One training epoch took {end_tr - start} seconds\n")
 
 
@@ -274,7 +274,7 @@ for epoch in range(n_epochs):
     # avg_ema_val_loss = ema_val_loss/len(val_loader)
     # ema_val_losses.append(avg_ema_val_loss)
 
-    with open("quick_test.txt", "a") as f:  # 'a' mode appends to the file
+    with open("thesis_train_filtered.txt", "a") as f:  # 'a' mode appends to the file
         f.write(f"One validation epoch took {time.time() - end_tr} seconds\n")
 
 
@@ -291,13 +291,13 @@ for epoch in range(n_epochs):
             'val_loss': best_val_loss,
         }, save_path)
 
-    with open("quick_test.txt", "a") as f:  # 'a' mode appends to the file
+    with open("thesis_train_filtered.txt", "a") as f:  # 'a' mode appends to the file
         f.write(f"Epoch {epoch + 1}/{n_epochs}, Loss: {avg_val_loss}\n")
 
     print(f'Epoch {epoch+1}/{n_epochs}, Validation Loss: {avg_val_loss}')
 
-    np.save(savedir+'/losses/train_loss_quicktest', train_losses)
-    np.save(savedir+'/losses/val_loss_quicktest', val_losses)
+    np.save(savedir+'/losses/train_loss_thesis_atm', train_losses)
+    np.save(savedir+'/losses/val_loss_thesis_atm', val_losses)
     # np.save(savedir+'/losses/ema_val_loss_ema_big_dataset', ema_val_losses)
 
 # Test phase (after all epochs)
