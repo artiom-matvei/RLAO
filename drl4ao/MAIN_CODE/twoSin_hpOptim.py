927ec5b6b24828c7b8b3e0ea4e57dab60edac8dd
Parker Levesque <parker09@narval1.narval.calcul.quebec>
2025-01-31 13:26:12 -0500

    Add timeout value to print summary before CC timeout

--- drl4ao/MAIN_CODE/twoSin_hpOptim.py
+++ drl4ao/MAIN_CODE/twoSin_hpOptim.py
@@ -398,7 +398,7 @@ def objective(trial):
 if __name__ == "__main__":
     
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=100, timeout=3600)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[optuna.tiral.TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[optuna.trial.TrialState.COMPLETE])
